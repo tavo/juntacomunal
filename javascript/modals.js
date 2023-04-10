78c4cf76046cc8d5b470b414c453ea547b935eab
Gustavo Delgado <gustavo.delgado@fullscript.com>
2023-04-10 16:28:28 -0400

    victoria de Alfredo

--- javascript/modals.js
+++ javascript/modals.js
@@ -1,12 +1,15 @@
 document.onreadystatechange = function () {
   if (document.readyState == "complete") {
     // Get the modal
+    var modalAlfredo = document.getElementById("final-alfredo");
     var modalJose = document.getElementById("final-jose");
     var modalRafa = document.getElementById("final-rafa");
     // Get the button that opens the modal
+    var btnAlfredo = document.getElementById("final-alfredo-button");
     var btnJose = document.getElementById("final-jose-button");
     var btnRafa = document.getElementById("final-rafa-button");
     // Get the <span> element that closes the modal
+    var spanAlfredo = document.getElementById("final-alfredo-close");
     var spanJose = document.getElementById("final-jose-close");
     var spanRafa = document.getElementById("final-rafa-close");
 
@@ -43,6 +46,25 @@ document.onreadystatechange = function () {
     window.onclick = function(event) {
       if (event.target == modalRafa) {
         modalRafa.style.display = "none";
+      }
+    }
+
+    // Alfredo
+
+    // When the user clicks on the button, open the modal
+    btnAlfredo.onclick = function() {
+      modalAlfredo.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    spanAlfredo.onclick = function() {
+      modalAlfredo.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+      if (event.target == modalAlfredo) {
+        modalAlfredo.style.display = "none";
       }
     }
   }
